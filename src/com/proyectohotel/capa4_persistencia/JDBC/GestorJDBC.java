b6b431eae04144c272564154399597bdecf9caa0
bruno <bruno@asd>
2020-10-18 19:46:42 -0500

    ADD gestor JDBC

---
+++ src/com/proyectohotel/capa4_persistencia/JDBC/GestorJDBC.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.proyectohotel.capa4_persistencia.JDBC;
+
+/**
+ *
+ * @author USER
+ */
+public abstract class GestorJDBC {
+    
+}
