5f4651c1a53d1f4452f681f1d1608521ff5f09bd
Dash166 <joseluis_1555@hotmail.com>
2020-10-21 11:12:52 -0500

    fix file gestorjdbc 2

--- src/com/proyectohotel/capa4_persistencia/jdbc_postgre/GestorJDBCPostgre.java
+++ src/com/proyectohotel/capa4_persistencia/jdbc_postgre/GestorJDBCPostgre.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.proyectohotel.capa4_persistencia.jdbc.GestorJDBCPostgre;
+package com.proyectohotel.capa4_persistencia.jdbc_postgre;
 
+import Config.Color;
 import com.proyectohotel.capa4_persistencia.JDBC.GestorJDBC;
-import java.awt.Color;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -15,16 +15,16 @@ import java.sql.SQLException;
  * @author josel
  */
 public class GestorJDBCPostgre extends GestorJDBC{
+    @Override
     public void abrirConexion() throws Exception {
-        try {
+           try {
             Class.forName("org.postgresql.Driver");
             String url = "jdbc:postgresql://localhost:5432/biblioteca";
             DriverManager.getConnection(url, "postgres", "postgres");
         } catch (ClassNotFoundException | SQLException e) {
-            System.out.println(Color.RED + "Problema de Conexion");
+            System.out.println(Color.RED + "ERROR DE CONEXION");
             throw new Exception("Error en la conexion con la base de datos, consulte con el administrador.");
         }
-         
-        
     }
+    
 }
